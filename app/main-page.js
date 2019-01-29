dc6928e4f2eab2598e6e30e54d5dda5db5714282
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-29 16:37:34 +0100

    One way data binding

--- app/main-page.js
+++ app/main-page.js
@@ -1,1 +1,29 @@
-/* */
+/*
+var createViewModel = require("./main-view-model").createViewModel;
+
+function onNavigatingTo(args) {
+    var page = args.object;
+
+    page.bindingContext = createViewModel();
+}
+
+exports.onNavigatingTo = onNavigatingTo;
+*/
+
+
+// Event handler function fired by navigatingTo Page tag attribute
+exports.theFunction = args => {
+    console.log("Main event handler function");
+    const page = args.object;
+    // Set the page binding context
+    page.bindingContext = {
+        message: "Hello from the code behind file!!"
+    };
+
+    setTimeout(() => {
+        page.bindingContext = {
+            message: "Time is off. Something happened and change in the logic"
+        };  
+    }, 5000);
+
+}

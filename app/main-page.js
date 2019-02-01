c9759597c14419cabd6cb2d8020ed4a8b7164972
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-02-01 10:31:31 +0100

    #18: fromObjectRecursive (two way data binding)

--- app/main-page.js
+++ app/main-page.js
@@ -1,19 +1,24 @@
-const { Observable, fromObject } = require("tns-core-modules/data/observable");
+const { Observable, fromObject, fromObjectRecursive } = require("tns-core-modules/data/observable");
 
 const theModel = {
-    "username": "",
-    "password": ""
+    "user" : {
+        "username": "frizio",
+        "password": ""
+    }
 };
-const bindingContext = fromObject(theModel);
+
+const bindingContext = fromObjectRecursive(theModel);
+
 
 exports.onLoaded = args => {
     console.log("The page is loading");
     const page = args.object;
-
     page.bindingContext = bindingContext;
 }
 
 exports.onTap = () => {
     console.log("Button TAP ME pressed");
-    alert(`Hey, ${bindingContext.get("username")}. Your password is ${bindingContext.get("password")} `);
+    let username = bindingContext.get("user").username; 
+    let password = bindingContext.get("user").password; 
+    alert(`Hey, ${username}. Your password is ${password}`);
 }
